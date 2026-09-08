7d6c300a79b4099fe541d9d1b23ee11d70742b9c
Abhijit Bangal <abhijit.bangal92@gmail.com>
2026-09-08 14:35:17 +0530

    changed invoice_relation to incremental load

--- dbt/models/transform/invoice_relation.sql
+++ dbt/models/transform/invoice_relation.sql
@@ -1,7 +1,20 @@
 {{
 config(
 materialized = 'incremental',
-transient = true
+incremental_strategy = 'append',
+transient = false
 )
 }}
-select * from {{ source('postgres','invoice_relation') }}
+select ID
+        ,INVOICE_ID
+        ,EXTERNAL_ID
+        ,CREATED_AT CREATED_DT
+        ,UPDATED_AT UPDATED_DT
+        ,DELETED_AT DELETED_DT
+        ,DATE(TRANSFORM_TS) LOAD_DT
+
+
+from {{ source('postgres','invoice_relation') }} C
+{% if is_incremental() %}
+where date(c.TRANSFORM_TS) > (select coalesce(max(t.load_dt),date('2010-01-01')) from {{ this }} t) 
+{% endif %}
