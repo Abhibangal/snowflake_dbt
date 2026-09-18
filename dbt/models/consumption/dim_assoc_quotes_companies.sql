7d46b736a243eaa740392664bbfca09507b44c28
Abhijit Bangal <abhijit.bangal92@gmail.com>
2026-09-18 12:24:28 +0530

    added the dim and fact table with rectified changes

--- dbt/models/consumption/dim_assoc_quotes_companies.sql
+++ dbt/models/consumption/dim_assoc_quotes_companies.sql
@@ -7,10 +7,6 @@ unique_key=['quote_id','company_id'],
 )
 }}
 
-select  * 
-        ,current_date() as load_dt
+select  distinct * 
+        
 from {{ ref('assoc_quotes_companies') }}
-qualify row_number() over (
-    partition by quote_id, company_id
-    order by updated_dt desc
-) = 1
--- dbt/models/consumption/dim_assoc_quotes_deals.sql
+++ dbt/models/consumption/dim_assoc_quotes_deals.sql
@@ -6,10 +6,6 @@ incremental_strategy = 'merge',
 unique_key=['quote_id','deal_id'],
 )
 }}
-select  * 
-        ,current_date() as load_dt
+select  distinct * 
+        
 from {{ ref('assoc_quotes_deals') }}
-qualify row_number() over (
-    partition by quote_id, deal_id
-    order by updated_dt desc
-) = 1
